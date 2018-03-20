e26972c5251f0ecf72e1eba1444f11fa1dffded1
Nguyen Van Thieu <thieumao@gmail.com>
2018-03-21 00:19:08 +0700

    Play Mp3

---
+++ Pods/Target Support Files/Pods-TalkEnglish/Pods-TalkEnglish-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TalkEnglish : NSObject
+@end
+@implementation PodsDummy_Pods_TalkEnglish
+@end
--- TalkEnglish/ViewController.swift
+++ TalkEnglish/ViewController.swift
@@ -7,19 +7,35 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var bombSoundEffect: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
+// IBAction - button click event
+extension ViewController {
+    @IBAction func playButtonClicked(_ sender: Any) {
+        playDemo("example.mp3")
+    }
+}
+
+// Play MP3
+extension ViewController {
+    func playDemo(_ fileName: String) {
+        let path = Bundle.main.path(forResource: fileName, ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            // couldn't load file :(
+        }
+    }
+}
